adc7a92dabb93a934b445f865d46c7ec6dbe06fb
Gabriel Canci <104606399+bielcanci@users.noreply.github.com>
2023-11-13 22:17:14 -0300

    Update DemoApplication.java

--- JogoDaVelha/src/main/java/com/example/demo/DemoApplication.java
+++ JogoDaVelha/src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,18 @@
+// Pacote da aplicação
 package com.example.demo;
 
+// Importações necessárias do Spring Boot
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// Anotação indicando que esta é uma aplicação Spring Boot
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    // Método principal, ponto de entrada da aplicação
+    public static void main(String[] args) {
+        // Inicia a aplicação Spring Boot
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
+
